835cae3c348d6e3561f9b620df918a0979c6d44c
Charles1989mds <162193121+Charles1989mds@users.noreply.github.com>
2024-10-22 14:09:44 -0300

    Add files via upload

---
+++ FLASHCARD/perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Inglaterra?',
+    'A capital da Inglaterra é Londres'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual o número aproximado de pi?',
+    'O número aproximado é 3,14'
+)
+
+criaCartao(
+    'Física',
+    'Qual a unidade de medida de calor, de acordo com o S.I?',
+    'A unidade de medida é o joule, representado por (j)'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual o total de cromossos nos seres humanos?',
+    'Os seres humanos possuem um total de 46 cromossomos'
+)
+
+criaCartao(
+    'Química',
+    'De acordo com a tabela periódica existem quantos elementos químicos?',
+    'Existem 118 elementos químicos'
+)
